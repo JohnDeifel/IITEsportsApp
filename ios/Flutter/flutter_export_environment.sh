1f0fcdbc70cdcefde14aac3ddb10ed906b962ca7
JohnDeifel <jhdeifel@gmail.com>
2023-10-23 23:17:47 -0500

    Fixing Problems

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/john/Documents/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/john/Documents/Development/vscode/Codify/esports_app/esports"
+export "FLUTTER_APPLICATION_PATH=/Users/john/Documents/Development/vscode/IITEsportsApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
